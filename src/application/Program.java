b5bab808a661cac0a995e431574796c59ff68ad4
Francisco siarra <Fcosiarra@gmail.com>
2020-07-31 14:23:39 -0700

    Project create

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+
+package application;
+
+/**
+ * @author francisco
+ */
+public class Program {
+    
+     public static void main(String[] args){
+         
+         System.out.println("Hello Word :");
+         
+     }
+    
+}
