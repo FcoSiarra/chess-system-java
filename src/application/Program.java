9074da2612cac2fb0830912fcb3da49909dd9a63
Francisco siarra <Fcosiarra@gmail.com>
2020-08-01 10:47:32 -0700

    first class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 
 package application;
 
+import boardgame.Position;
+
 /**
  * @author francisco
  */
@@ -8,7 +10,9 @@ public class Program {
     
      public static void main(String[] args){
          
-         System.out.println("Hello Word :");
+         Position pos = new Position (5,3);
+         
+         System.out.println(pos);
          
      }
     
